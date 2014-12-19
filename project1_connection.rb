bb39cd18a56f645a752ed6922b0e6af1d6681f3e
umerdar <umerbashirdar@gmail.com>
2014-12-19 11:25:43 -0500

    first commit

---
+++ project1_connection.rb
@@ -0,0 +1,10 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection({
+  :adapter => "postgresql",
+  :host => "localhost",
+  :username => "umerdar",
+  :database => "got"
+  })
+
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
